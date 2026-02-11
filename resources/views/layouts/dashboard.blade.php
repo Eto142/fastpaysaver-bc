ea0e474fdedc2ecbe3707e77e538b421e2fc511c
eto <eto.saviour80@gmail.com>
2026-02-11 19:50:38 +0000

    update

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -13,7 +13,7 @@
     {{-- Google Font --}}
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     {{-- Dashboard CSS --}}
-    <link href="{{ asset('assets/css/bank-dashboard.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/bank-dashboard.css') }}?v={{ filemtime(public_path('assets/css/bank-dashboard.css')) }}" rel="stylesheet">
     @stack('styles')
 </head>
 <body class="loading">
